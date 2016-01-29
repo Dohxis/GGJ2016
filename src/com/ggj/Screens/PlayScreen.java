8d03ed9920c8caf1fd4cfb9357f5310692349a10
Dohxis <dohxis@gmail.com>
2016-01-29 23:12:20 +0200

    PlayScreen()

---
+++ src/com/ggj/Screens/PlayScreen.java
@@ -0,0 +1,53 @@
+package com.ggj.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.ggj.Game;
+
+public class PlayScreen implements Screen {
+
+    Game game;
+
+    public PlayScreen(Game game){
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
