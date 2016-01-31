1e10aded8fc555aeb9cdb99ebf1ecb3ef44c264f
Dohxis <dohxis@gmail.com>
2016-01-31 15:04:54 +0200

    MUSIC

--- src/com/ggj/Game.java
+++ src/com/ggj/Game.java
@@ -1,6 +1,7 @@
 package com.ggj;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,11 +11,18 @@ import com.ggj.Screens.WinLose;
 
 public class Game extends com.badlogic.gdx.Game {
 	public SpriteBatch batch;
+	Music music;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		setScreen(new MenuScreen(this));
+
+		music = Gdx.audio.newMusic(Gdx.files.internal("musi.mp3"));
+
+		music.setVolume(0.05f);
+		music.setLooping(true);
+		music.play();
 	}
 
 	@Override
